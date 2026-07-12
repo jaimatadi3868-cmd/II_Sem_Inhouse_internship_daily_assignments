1d20df8e348b0b1222fc89027946f3d5e4a2ff76
jaimatadi3868-cmd <jaimatadi3868@gmail.com>
2026-07-12 21:50:37 +0530

    Add database connection script in db_connect1.php

---
+++ 25ESKCA042/DAY 9/db_connect1.php
@@ -0,0 +1,15 @@
+<?php
+
+$host   = "localhost";
+$user   =  "root";
+$password  = "1234567" ;
+$database  = "student_management" ;
+
+$conn = mysqli_connect($host, $user, $password,$database);
+
+if (!$conn) {
+    die ("connection failed:" . mysqli_connect_error());
+}
+ echo "connection  successfull ";
+ 
+?>
